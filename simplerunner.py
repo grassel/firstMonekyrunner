2e18a40413548b39e44b93aaf9fda6f13326af53
Guido Grassel <guido@grassel.org>
2017-11-21 19:52:42 +0200

    launch and enterURL functions added

--- simplerunner.py
+++ simplerunner.py
@@ -1,8 +1,50 @@
-import time    
 
 # Imports the monkeyrunner modules used by this program
 from com.android.monkeyrunner import MonkeyRunner, MonkeyDevice
 
+
+
+
+
+# launches the browser, if it is not running, yet
+# raises browser to foreground if it is running already
+def launchChrome():
+    # info from https://stackoverflow.com/questions/28150650/open-chrome-with-adb
+    device.startActivity(component='com.android.chrome/com.google.android.apps.chrome.Main')
+    MonkeyRunner.sleep(3.0)
+    return
+
+# launches the browser, if it is not running, yet
+# raises browser to foreground if it is running already
+def launchHWBrowser():
+    device.startActivity(component='com.huawei.browser/com.google.android.apps.chrome.Main')
+    MonkeyRunner.sleep(3.0)
+    return
+
+
+# enters given URL to the URL bar and hits enter key
+def enterURL(u):
+   device.wake()
+
+   # we might be in the middle of the page and URL bar not visible
+   # scroll the page just enough to make sure URL bar is visible
+   device.drag ((400, 600), (400, 590), steps=5, duration=0.2)
+   MonkeyRunner.sleep(0.5)
+
+    # move focus to the URL box
+   device.touch(250, 157, MonkeyDevice.DOWN_AND_UP) 
+
+   # all text in the URL bar will be selected
+   # hit DEL to clear URL bar
+   device.press('KEYCODE_DEL', MonkeyDevice.DOWN_AND_UP)
+
+   #enter URL and hit 'enter' 
+   device.type(u + '\n')
+   return
+
+
+
+   
 # Connects to the current device, returning a MonkeyDevice object
 device = MonkeyRunner.waitForConnection()
 
@@ -10,20 +52,9 @@ device = MonkeyRunner.waitForConnection()
 # to see if the installation worked.
 #device.installPackage('myproject/bin/MyApplication.apk')
 
-# info from https://stackoverflow.com/questions/28150650/open-chrome-with-adb
 
-# sets a variable with the package's internal name
-package = 'com.android.chrome'
+launchHWBrowser()
 
-# sets a variable with the name of an Activity in the package
-activity = 'com.google.android.apps.chrome.Main'
-
-# sets the name of the component to start
-runComponent = package + '/' + activity
-
-# Runs the component
-device.startActivity(component=runComponent)
-time.sleep(3.0)
 
 # Presses the Menu button
 #device.press('KEYCODE_MENU', MonkeyDevice.DOWN_AND_UP)
@@ -34,28 +65,10 @@ time.sleep(3.0)
 # Writes the screenshot to a file
 #result.writeToFile('./shot1.png','png')
 
+enterURL('https://www.grassel.org/')
+MonkeyRunner.sleep(3.0)
 
-device.drag ((400, 900), (200, 100), steps=400, duration=3.0)
-device.drag ((200, 100), (200, 900), steps=400, duration=2.0)
-
-# from https://gist.github.com/sjp38/6202539
-
-# Touch down screen                                                                               
-device.touch(100, 500, MonkeyDevice.DOWN)                                           
-
-# Move from 100, 500 to 300, 500
-for i in range(1, 11):                                                              
-    device.touch(100, 800-50*i, MonkeyDevice.MOVE)                              
-    print "move ", 100 + 20 * i, 500                                                
-    time.sleep(0.1)             
-
-# Move from (300, 500 to 200, 500)                                                    
-for i in range(1, 11):                                                              
-    device.touch(300, 500 - 10 * i, MonkeyDevice.MOVE)                              
-    print "move ", 300, 500 - 10 * i                                                
-    time.sleep(0.1)                                                                 
-
-# Remove finger from screen
-device.touch(300, 400, MonkeyDevice.UP) 
+#device.drag ((400, 900), (200, 100), steps=400, duration=3.0)
+#device.drag ((200, 100), (200, 900), steps=400, duration=2.0)
 
 
